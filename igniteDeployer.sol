fe11d27b4b7515cbcc0b411d58216440c1a1c198
Ignite Blockchain <97672861+ignitechain@users.noreply.github.com>
2022-02-23 15:12:21 +0530

    Update igniteDeployer.sol

--- igniteDeployer.sol
+++ igniteDeployer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
 contract Context {
     function _msgSender() internal view virtual returns (address) {
@@ -29,36 +29,16 @@ contract Context {
 
 contract igniteDeployer is Ownable  {
 
-    uint256 private pass = 999999999;
-    address public admin;
-    mapping(address => bool) public authorized;
+    address private _owner;
 
     constructor() {
-        admin = msg.sender;
+        _owner = msg.sender;
     }
-
-     modifier onlyAuthorized()  {
-        require(authorized[msg.sender] || owner() == msg.sender);
-        _;
-    }
-
-    function addAuthorized(address _toAdd) onlyOwner public {
-        require(_toAdd != address(0));
-        authorized[_toAdd] = true;
-    }
-
-    function removeAuthorized(address _toRemove) onlyOwner public {
-        require(_toRemove != address(0));
-        require(_toRemove != msg.sender);
-        authorized[_toRemove] = false;
-    }
-    
    function depositIGT() external payable{}
-    function withdrawIGT(address payable recipeint, uint256 value,uint256  transactionKey) external  {    
-        require(msg.sender ==  admin || authorized[msg.sender] == true);
-        require(pass == transactionKey);
-        require(msg.sender == recipeint);
-        recipeint.transfer(value);
+    function withdrawIGT(uint256 value) external  {    
+        require(msg.sender ==  _owner);
+
+        payable(msg.sender).transfer(value);
     }
     function igtBalance() external view returns(uint){
         return address(this).balance;
